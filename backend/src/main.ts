cf8a2be07540b3ce6afd4511171998b1ff7295cb
bodyabandura <bohdan.bandura.work@gmail.com>
2025-06-02 14:26:27 +0300

    fixings cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: [
+      'https://video-search-app-azure.vercel.app',
+      'http://localhost:5173',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
